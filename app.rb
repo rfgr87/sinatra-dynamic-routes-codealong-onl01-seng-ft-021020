54a063aca70bd41b9e86ad5bc6af24fa9e13a759
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-23 23:23:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,4 +20,9 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
 
+  get '/multiply/:num1/:num2' do 
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    @product = @num1.to_i * @num2.to_i
+    @poduct.to_s
 end
